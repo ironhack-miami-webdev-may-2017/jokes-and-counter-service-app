6fbe241ab0d25e1df31eb652b8b0896eb684d71f
Nicholas Borbe <nicholasborbe@Nicholass-MacBook-Pro-2.local>
2017-07-17 11:02:02 -0400

    added joke service & joke componenet + 2 separate routes for counter vs jokes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 //imports our comnponent into the file
-import {AppComponent} from './app.component';
+import {AppComponent  } from './app.component';
+import {JokesComponent} from './jokes/jokes.component';
+import {BothCountersComponent} from './both-counters/both-counters.component';
+
+
 
 
 // we set all paths to point to AppComponent
 const routes: Routes = [
   {
-    path: '',
-    component: AppComponent
+    path: 'counter',
+    component: BothCountersComponent
+  },
+  {
+    path: 'jokes',
+    component: JokesComponent
   }
 ];
 
--- src/app/my-counter-2/my-counter-2.component.ts
+++ src/app/my-counter-2/my-counter-2.component.ts
@@ -3,15 +3,15 @@ import { CounterService } from '../counter.service';
 
 
 @Component({
-  selector: 'app-my-counter',
-  templateUrl: 'my-counter.component.html',
+  selector: 'app-my-counter-2',
+  templateUrl: 'my-counter-2.component.html',
   styles: [],
-  // providers option is for creating a separate instance of the
+  // providers option is for creating a separate instance of the 
   //service in each component
   // providers: [CounterService]
 })
 
-export class MyCounterComponent implements OnInit {
+export class MyCounter2Component implements OnInit {
   constructor(private theCounter: CounterService) {}
 
   ngOnInit() {}
