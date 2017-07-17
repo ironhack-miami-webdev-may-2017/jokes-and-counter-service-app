d783d2451b553b1283d8834c1537b3bd14b4afc1
Nicholas Borbe <nicholasborbe@Nicholass-MacBook-Pro-2.local>
2017-07-17 09:38:23 -0400

    added counter service and first counter component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//imports our comnponent into the file
+import {AppComponent} from './app.component';
+
+
+// we set all paths to point to AppComponent
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: AppComponent
   }
 ];
 
